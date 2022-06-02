30f192c4a1585d5f95fd472a16ca94623e34a9d1
vhenriqueDev <vhenriquesantos0@gmail.com>
2022-06-02 10:34:48 -0300

    aula 02

---
+++ codigos-e-desafios/Módulo-III-Javascript/Sintaxe-Básica-em-JavaScript/aula02/script.js
@@ -0,0 +1,100 @@
+// tipos primitivos
+
+// boolean
+/*var VouF = false
+console.log(typeof VouF)
+
+// number
+var numeroqualquer = 1
+console.log(typeof numeroqualquer)
+
+// string
+var nome = 'victor'
+console.log(typeof nome)
+
+// function
+var funcao = function () {}
+console.log(typeof funcao)
+
+// como declarar
+var variavel = 'victor'
+variavel = 'henrique'
+console.log(variavel)
+
+let variavel2 = 'victor'
+variavel = 'henrique'
+console.log(variavel2)
+
+const constante = 'victor'
+// constante = 'henrique'
+console.log(constante)
+
+var escopoGlobal = 'global'
+console.log(escopoGlobal)
+
+function escopoLocal() {
+  let escopoLocalInterno = 'local'
+  console.log(escopoLocalInterno)
+}
+
+escopoLocal()
+
+// atribuição
+var atribuicao = 'victor'
+
+// comparacão
+var comparacao = '0' == 0
+console.log(comparacao)
+
+// comparacao identica
+var comparacaoIdentica = '0' === 0
+console.log(comparacaoIdentica)
+
+// adicão
+var adicao = 1 + 1
+console.log(adicao)
+
+// subtracão
+var subtracao = 2 - 1
+console.log(subtracao)
+
+// multiplicação
+var multiplicacao = 2 * 3
+console.log(multiplicacao)
+
+// divisão real
+var divisaoReal = 6 / 2
+console.log(divisaoReal)
+
+// divisão inteira
+var divisaoInteira = 5 % 2
+console.log(divisaoInteira)
+
+//  potenciação
+var potenciacao = 2 ** 10
+console.log(potenciacao)
+
+// maior que
+var maiorQue = 5 > 2
+console.log(maiorQue)
+
+// menor que
+var menorQue = 5 < 2
+console.log(menorQue)
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2
+console.log(maiorOuIgual)
+
+// menor ou igual a
+var menorOuIgual = 5 <= 2
+console.log(menorOuIgual)
+
+var e = true && true
+console.log(e)
+
+var ou = true || false
+console.log(ou)
+
+var nao = !true
+console.log(nao)*/
